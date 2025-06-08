0c6a266ba3977564a403286c1c03c9f64660a04d
MOHAMMAD IMRON ROSYADI <imronrsya@gmail.com>
2025-06-08 17:37:58 +0700

    Configure Vite for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ToDoList/', // Ganti dengan nama repository GitHub Anda
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist'
   }
 })
