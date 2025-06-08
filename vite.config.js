0ae12ea94a38758732d6102461e28d27991e3eac
MOHAMMAD IMRON ROSYADI <imronrsya@gmail.com>
2025-06-08 17:41:00 +0700

    Fix GitHub Actions workflow and update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ToDoList/', // Ganti dengan nama repository GitHub Anda
+  base: '/To-Do-List/', // Sesuai dengan nama repository GitHub
   server: {
     port: 3000,
     open: true
